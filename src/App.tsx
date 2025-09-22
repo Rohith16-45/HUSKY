ffc739360c6eefe4abffbb84ce73775ea2911a48
Rohith16-45 <rohithyuvasoft469@gmail.com>
2025-09-22 14:29:35 +0530

    fixed-husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import "./App.css";
 
 function App() {
   const name = "Rohith";
-
-  const legend = "me";
-
-  console.log(legend);
-
-  console.log(age);
-
   return (
     <div>
       <h1>Husky Implement {name}</h1>
